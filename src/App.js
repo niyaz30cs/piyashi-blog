abbd72a4f95a4f3782c0d63f9927129c7ce0d80c
Niyaz Alam <110074633+niyaz30cs@users.noreply.github.com>
2023-10-21 13:01:02 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+import RouterCompo from './Route/RouterCompo';
+import "../src/style.css"
+// import Footer from './Route/Footer';
+// import FooterCompo from './Component/FooterCompo';
+// import  Footer from "../src/Route/Footer"
+// import FooterCompo from './Component/FooterCompo';
+
+function App() {
+  // console.log(<FooterCompo /> )
+  return (
+    <div className="App">
+     <RouterCompo /> 
+     {/* <FooterCompo /> */}
+    
+    </div>
+  );
+}
+
+export default App;
